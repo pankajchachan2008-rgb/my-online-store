a6d648035afa264e52e46a01d04805012fbdb457
Aapka Naam <aapka_email@example.com>
2026-07-17 16:37:07 +0530

    Fixed Gmail App Password for OTP

--- store_backend/settings.py
+++ store_backend/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pankajchachan2026@gmail.com'
-EMAIL_HOST_PASSWORD = 'qovgaxoytqwlrtaz'  # Apne App Password se replace karein
+EMAIL_HOST_PASSWORD = 'oltqxsmoydkhgoah'  # Apne App Password se replace karein
