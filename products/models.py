5a5eeb03d4108fa427c552116b91086cfa6e6c6e
Aapka Naam <aapka_email@example.com>
2026-08-05 15:12:08 +0530

    Added stock field to Product model and updated ERP POS features

--- products/models.py
+++ products/models.py
@@ -35,7 +35,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     
     mrp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) 
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    stock = models.IntegerField(default=0) # 🌟 Yeh naya field add karein 
     
     color = models.CharField(max_length=50, blank=True, null=True, help_text="e.g., Black, White, Red, Blue")
     size = models.CharField(max_length=50, blank=True, null=True, help_text="General Size e.g., S, M, L, XL (Filter ke liye)")
