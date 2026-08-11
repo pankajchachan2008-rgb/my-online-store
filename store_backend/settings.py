f3fd0ae9fb0a35c2dc019197b8e33166266a203f
Aapka Naam <aapka_email@example.com>
2026-08-11 14:14:28 +0530

    setting and security

--- store_backend/settings.py
+++ store_backend/settings.py
@@ -14,10 +14,14 @@ if not SECRET_KEY:
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Allowed hosts and trusted corporate endpoints
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+        'cgsmart.in', 
+        'www.cgsmart.in', 
+        'my-online-store-ggbj.onrender.com', # Yahan apna Render wala exact domain zaroor check kar lein
+]
 
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
@@ -121,23 +125,19 @@ STORAGES = {
 }
 
 # =====================================================================
-#                  SECURITY HARDENING & CSP SETTINGS
+#                 SECURITY HARDENING & CSP SETTINGS
 # =====================================================================
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# Smart SSL Settings: Local pe HTTP, Live pe HTTPS
-SECURE_SSL_REDIRECT = not DEBUG
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
-if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-
-SESSION_COOKIE_SECURE = not DEBUG
-CSRF_COOKIE_SECURE = not DEBUG
-
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = 'Lax'
